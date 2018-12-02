42ce3780f2d7fcecbed370f7e2949f5112b379a5
Daniel Zhang <daniel.zhang@berkeley.edu>
2018-12-02 19:08:58 +0000

    make some changes in first time run

--- api/app/apps.py
+++ api/app/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+import sys
+sys.path.append("../")
+from bluepy import btle
 
 class AppConfig(AppConfig):
     name = 'app'
+    print("Connecting...")
+    device = btle.Peripheral("MAC:ADDRESS:1")
