2faab7ef399be004dd2fdd116121cf8a9b57240c
Daniel <daniel.zhang@berkeley.edu>
2018-12-04 11:41:04 -0800

    iterate over saved lights to connect

--- api/app/apps.py
+++ api/app/apps.py
@@ -2,12 +2,16 @@ from django.apps import AppConfig
 import sys
 sys.path.append("../")
 from bluepy import btle
+from app.models import Light
 
 device = None
 
 class AppConfig(AppConfig):
     name = 'app'
     print("Connecting...")
-    global device 
-    device = btle.Peripheral("D3:45:72:1D:0C:05", btle.ADDR_TYPE_RANDOM)
+    global devices = []
+    for light in Lights.objects.all():
+        if len(light.lightMAC) > 0:
+            device = btle.Peripheral(light.lightMAC, btle.ADDR_TYPE_RANDOM)
+            devices.append(device)
     print(device.getServices())
