3beba6dec37bfc8aa02042577a3064c6bac0d357
Daniel Zhang <daniel.zhang@berkeley.edu>
2018-12-04 20:02:27 +0000

    change some stuff in apps

--- api/app/apps.py
+++ api/app/apps.py
@@ -9,10 +9,13 @@ device = None
 class AppConfig(AppConfig):
     name = 'app'
     print("Connecting...")
-    global devices = []
-    for light in Lights.objects.all():
-        if len(light.lightMAC) > 0:
-            device = btle.Peripheral(light.lightMAC, btle.ADDR_TYPE_RANDOM)
-            print(device.getServices())
+    def ready(self):
+        global devices
+        devices = []
 
-            devices.append(device)
+        for light in Lights.objects.all():
+            if len(light.lightMAC) > 0:
+                device = btle.Peripheral(light.lightMAC, btle.ADDR_TYPE_RANDOM)
+                print(device.getServices())
+
+                devices.append(device)
