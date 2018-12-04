6cc7ba0064c88951f43075caa54cc013011bf731
Daniel <daniel.zhang@berkeley.edu>
2018-12-04 11:42:01 -0800

    fix error in apps.py

--- api/app/apps.py
+++ api/app/apps.py
@@ -13,5 +13,6 @@ class AppConfig(AppConfig):
     for light in Lights.objects.all():
         if len(light.lightMAC) > 0:
             device = btle.Peripheral(light.lightMAC, btle.ADDR_TYPE_RANDOM)
+            print(device.getServices())
+
             devices.append(device)
-    print(device.getServices())
