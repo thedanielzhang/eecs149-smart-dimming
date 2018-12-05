c9b5d4eb3f7b93fa4c47f9c4fffd5db9df34115b
hteng1995 <hteng1995@berkeley.edu>
2018-12-05 15:39:32 -0800

    added buck R/W for BLE

--- ac-ble/our_service.c
+++ ac-ble/our_service.c
@@ -6,27 +6,87 @@
 #include "ble_srv_common.h"
 #include "app_error.h"
 
+static void on_write(ble_os_t * p_our_service, ble_evt_t const * p_ble_evt)
+{
+    ble_gatts_evt_write_t const * p_evt_write = &p_ble_evt->evt.gatts_evt.params.write;
+
+    // Custom Value Characteristic Written to.
+    if (p_evt_write->handle == p_our_service->char_handles.value_handle)
+    {
+        uint16_t char_data = *p_evt_write->data;
+        printf("Characteristic written to. Value: 0x%X\n", char_data);
+        
+        /* Char data:
+         * xxxccsssllllllll
+         *
+         * l = light level bit
+         * s = source bit
+         * c = config bit
+         *     - lo bit = motion tracking
+         *     - hi bit = light tracking
+         * x = not used
+         *
+         */
+        //uint8_t new_light_level = char_data & 0xFF;
+        //uint8_t source = (char_data >> 8) & 0x7;
+        //bool motion_enabled = (char_data >> 11)  & 1;
+        //bool light_tracking_enabled = (char_data >> 12) & 1;
+        //do what we will...
+    }
+    /*
+    // Check if the Custom value CCCD is written to and that the value is the appropriate length, i.e 2 bytes.
+    if ((p_evt_write->handle == p_our_service->custom_value_handles.cccd_handle)
+        && (p_evt_write->len == 2)
+       )
+    {
+        // CCCD written, call application event handler
+        if (p_cus->evt_handler != NULL)
+        {
+            ble_cus_evt_t evt;
+
+            if (ble_srv_is_notification_enabled(p_evt_write->data))
+            {
+                printf("Notifications enabled\n");
+                evt.evt_type = BLE_CUS_EVT_NOTIFICATION_ENABLED;
+            }
+            else
+            {
+                printf("Notifications disabled\n");
+                evt.evt_type = BLE_CUS_EVT_NOTIFICATION_DISABLED;
+            }
+            // Call the application event handler.
+            p_cus->evt_handler(p_cus, &evt);
+        }
+    }
+    */
+
+}
+
 void ble_our_service_on_ble_evt(ble_evt_t const * p_ble_evt, void * p_context)
 {
-  	ble_os_t * p_our_service =(ble_os_t *) p_context;  
-		//Implement switch case handling BLE events related to our service. 
+  	ble_os_t * p_our_service =(ble_os_t *) p_context;
+		//Implement switch case handling BLE events related to our service.
            switch (p_ble_evt->header.evt_id)
     {
         case BLE_GAP_EVT_CONNECTED:
           p_our_service->conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
             break;
         case BLE_GAP_EVT_DISCONNECTED:
+          UNUSED_PARAMETER(p_ble_evt);
           p_our_service->conn_handle = BLE_CONN_HANDLE_INVALID;
+            break;
+        case BLE_GATTS_EVT_WRITE:
+            on_write(p_our_service, p_ble_evt);
             break;
       default:
           // No implementation needed.
           break;
     }
-	
+
 }
 
-// This Function is for adding our new characterstic to the setvice "lighting". 
- 
+// This Function is for adding our new characterstic to the setvice "lighting".
+
 static uint32_t our_char_add(ble_os_t * p_our_service)
 {
     // Adding a custom characteristic UUID
@@ -34,12 +94,21 @@ static uint32_t our_char_add(ble_os_t * p_our_service)
     ble_uuid_t          char_uuid;
     ble_uuid128_t       base_uuid = BLE_UUID_OUR_BASE_UUID;
     char_uuid.uuid      = BLE_UUID_BRIGHTNESS_CHARECTERISTIC;
-    err_code_brightness_char= sd_ble_uuid_vs_add(&base_uuid, &char_uuid.type);
-    APP_ERROR_CHECK(err_code_brightness_char);  
-
-    
+    err_code_brightness_char = sd_ble_uuid_vs_add(&base_uuid, &char_uuid.type);
+    APP_ERROR_CHECK(err_code_brightness_char);
 
 
+
+
+
+
+    // Configuring Client Characteristic Configuration Descriptor metadata and add to char_md structure
+    ble_gatts_attr_md_t cccd_md;
+    memset(&cccd_md, 0, sizeof(cccd_md));
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
+    cccd_md.vloc                = BLE_GATTS_VLOC_STACK;
 
     //Adding read/write properties to our characteristic
     ble_gatts_char_md_t char_md;
@@ -47,47 +116,33 @@ static uint32_t our_char_add(ble_os_t * p_our_service)
 
     char_md.char_props.read = 1;
     char_md.char_props.write = 1;
-
-    
-    // Configuring Client Characteristic Configuration Descriptor metadata and add to char_md structure
-    ble_gatts_attr_md_t cccd_md;
-    memset(&cccd_md, 0, sizeof(cccd_md));
-
-    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
-    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
-    cccd_md.vloc                = BLE_GATTS_VLOC_STACK;    
     char_md.p_cccd_md           = &cccd_md;
     char_md.char_props.notify   = 1;
-   
-    
+
+
     // Configuring the attribute metadata
     ble_gatts_attr_md_t attr_md;
-    memset(&attr_md, 0, sizeof(attr_md));  
+    memset(&attr_md, 0, sizeof(attr_md));
     attr_md.vloc        = BLE_GATTS_VLOC_STACK;
-    
-    
+
+
     //Setting read/write security levels to our characteristic
     BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
     BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
-    
-    
+
+
     //Configuring the characteristic value attribute
     ble_gatts_attr_t    attr_char_value;
     memset(&attr_char_value, 0, sizeof(attr_char_value));
 
     attr_char_value.p_uuid      = &char_uuid;
     attr_char_value.p_attr_md   = &attr_md;
-
-    
-
-    //Setting characteristic length in number of bytes
-
-    attr_char_value.max_len     = 4;
-    attr_char_value.init_len    = 4;
-    uint8_t value[4]            = {0x12,0x34,0x56,0x78};  // this is the initial value 0X12345678
+    attr_char_value.max_len     = 2;
+    attr_char_value.init_len    = 2;
+    uint8_t value[4]            = {0x00/*light_level*/,0x00/*3 bits: cause; 2 bits: config*/};
     attr_char_value.p_value     = value;
 
-    
+
     //Adding our new characteristic to the service
     err_code_brightness_char = sd_ble_gatts_characteristic_add(p_our_service->service_handle,
                                    &char_md,
@@ -110,38 +165,69 @@ static uint32_t our_char_add(ble_os_t * p_our_service)
     ble_uuid128_t     base_uuid = BLE_UUID_OUR_BASE_UUID;
     service_uuid.uuid = BLE_UUID_OUR_SERVICE_UUID;
     err_code = sd_ble_uuid_vs_add(&base_uuid, &service_uuid.type);
-    APP_ERROR_CHECK(err_code);    
-    
+    APP_ERROR_CHECK(err_code);
+
     // Setting our service connection handle to default value. I.e. an invalid handle since we are not yet in a connection.
-	
+
     p_our_service->conn_handle = BLE_CONN_HANDLE_INVALID;
 
     // Adding our service
 		err_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY,&service_uuid, &p_our_service->service_handle);
-    
+
     APP_ERROR_CHECK(err_code);
-    
-    // Calling the function our_char_add() to add our new characteristic to the service. 
+
+    // Calling the function our_char_add() to add our new characteristic to the service.
     our_char_add(p_our_service);
 }
 
 // Function to be called when updating characteristic value
-void our_temperature_characteristic_update(ble_os_t *p_our_service, int32_t *temperature_value)
+uint32_t our_characteristic_update(ble_os_t *p_our_service, uint16_t level, uint16_t source)
 {
+
+  if (p_our_service == NULL)
+  {
+      return NRF_ERROR_NULL;
+  }
+  bool motion_enabled = 1;
+  bool light_tracking_enabled = 1;
+  uint16_t char_value = (level & 0xFF) | (source << 8) | (motion_enabled << 11) | (light_tracking_enabled << 12);
+
+  uint32_t err_code = NRF_SUCCESS;
+  ble_gatts_value_t gatts_value;
+
+  // Initialize value struct.
+  memset(&gatts_value, 0, sizeof(gatts_value));
+
+  gatts_value.len     = 2;
+  gatts_value.offset  = 0;
+  gatts_value.p_value = (uint8_t *)&char_value;
+
+  // Update database.
+  err_code = sd_ble_gatts_value_set(p_our_service->conn_handle,
+                                    p_our_service->char_handles.value_handle,
+                                    &gatts_value);
+  if (err_code != NRF_SUCCESS)
+  {
+      return err_code;
+  }
     // Updating characteristic value
     if (p_our_service->conn_handle != BLE_CONN_HANDLE_INVALID)
   {
-      uint16_t               len = 4;
       ble_gatts_hvx_params_t hvx_params;
       memset(&hvx_params, 0, sizeof(hvx_params));
 
       hvx_params.handle = p_our_service->char_handles.value_handle;
       hvx_params.type   = BLE_GATT_HVX_NOTIFICATION;
-      hvx_params.offset = 0;
-      hvx_params.p_len  = &len;
-      hvx_params.p_data = (uint8_t*)temperature_value;  
+      hvx_params.offset = gatts_value.offset;
+      hvx_params.p_len  = &gatts_value.len;
+      hvx_params.p_data = gatts_value.p_value;
 
       sd_ble_gatts_hvx(p_our_service->conn_handle, &hvx_params);
+    } else {
+      err_code = NRF_ERROR_INVALID_STATE;
+      //NRF_LOG_INFO("sd_ble_gatts_hvx result: NRF_ERROR_INVALID_STATE. \r\n");
     }
+
+  return err_code;
 
 }
