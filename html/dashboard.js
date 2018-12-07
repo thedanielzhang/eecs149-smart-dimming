b6574be7ed23a38af496b9325389fe39db50823b
milodarling <milodarling@berkeley.edu>
2018-12-06 17:01:14 -0800

    Added AJAX requests

--- html/dashboard.js
+++ html/dashboard.js
@@ -1,30 +1,67 @@
-var slider_changed = function() {
-    var new_dim_level = 255 - slider.getValue();
-    //send it over bluetooth
-    console.log(new_dim_level);
-}
-
-var slider = $('#light-slide').slider().on('slide', slider_changed).data('slider');
-
-$('input#ambient-tracking-check').change(function() {
-    if ($('input#ambient-tracking-check').is(':checked')) {
-        console.log('ambient light checked');
-    } else {
-        console.log('ambient light unchecked');
-    }
-});
-
-$('input#motion-tracking-check').change(function() {
-    if ($('input#motion-tracking-check').is(':checked')) {
-        console.log('motion tracking checked');
-    } else {
-        console.log('motion tracking unchecked');
-    }
-});
-
-
-
 $(function() {
+    //we will probably actually use django templating for this
+    let searchParams = new URLSearchParams(window.location.search);
+    let light = searchParams.has('light') ? searchParams.get('light') : 0;
+
+    /*
+    var socket = new WebSocket("ws://www.example.com/socketserver");
+
+    socket.onopen = function(event) {
+        console.log("socket connected!");
+    }
+    */
+
+    var slider = $('#light-slide').slider().on('slide', slider_changed).data('slider')
+
+    $.get('/lights/'+light, function(data) {
+        console.log(data)
+        let info = JSON.parse(data);
+        if ("dim_level" in info) {
+            slider.setValue(info["dim_level"], false, false);
+        }
+        if ("light_tracking_enabled" in info && info["light_tracking_enabled"]) {
+            //check the box
+        }
+        if ("motion_tracking_enabled" in info && info["motion_tracking_enabled"]) {
+            //check the box
+        }
+    });
+
+    var slider_changed = function() {
+        var new_dim_level = 255 - slider.getValue();
+        //send it over bluetooth
+        console.log(new_dim_level);
+        //socket.send(JSON.stringify({"dim_level":new_dim_level}));
+        $.ajax({
+            url: '/lights/'+light,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify({"dim_level": new_dim_level})
+        });
+    }
+
+    //slider.on('slide', slider_changed);
+
+    $('input#ambient-tracking-check').change(function() {
+        let light_tracking_enabled = ($('input#ambient-tracking-check').is(':checked'));
+        $.ajax({
+            url: '/lights/'+light,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify({"light_tracking_enabled": light_tracking_enabled})
+        });
+    });
+
+    $('input#motion-tracking-check').change(function() {
+        let motion_tracking_enabled = ($('input#motion-tracking-check').is(':checked'));
+        $.ajax({
+            url: '/lights/'+light,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify({"light_tracking_enabled": light_tracking_enabled})
+        });
+    });
+
     scheduler.config.day_date = "%l";
     scheduler.init('scheduler_here', new Date(0),"week");
     scheduler.attachEvent('onEventAdded', function(id, ev) {
@@ -46,11 +83,6 @@ $(function() {
         {id:3, text:"Min: 20%\nMax: 80%", start_date:"01/01/1970 14:00",end_date:"01/01/1970 17:00"},
     ]/*{{ events|safe }}*/;
     scheduler.parse(events, "json");
-
-
-    //we will probably actually use django templating for this
-    let searchParams = new URLSearchParams(window.location.search);
-    let light = searchParams.has('light') ? searchParams.get('light') : 0;
 
     $('#light-'+light).addClass('active');
 });
