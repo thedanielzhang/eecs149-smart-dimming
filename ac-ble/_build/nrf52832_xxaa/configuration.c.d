d40ad2f1fcc9a9f9664f0616ccbc781ed3aa1401
Daniel Zhang <daniel.zhang@berkeley.edu>
2018-12-10 23:23:52 +0000

    remove build directories

--- ac-ble/_build/nrf52832_xxaa/configuration.c.d
+++
@@ -1,6 +0,0 @@
-_build/nrf52832_xxaa/configuration.c.o: configuration.c configuration.h \
- /usr/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h
-
-configuration.h:
-
-/usr/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h:
